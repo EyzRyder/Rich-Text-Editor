a822e2cb3483d7f6a27c453d36cdcc5fe5de5fe5
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-07-31 14:22:09 -0300

    Created Initial References

---
+++ script.js
@@ -0,0 +1,19 @@
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionsButtons = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let likeButton = document.getElementById("createLink");
+let alignButton = document.querySelectorAll(".align");
+let spacingButton = document.querySelectorAll(".spacing");
+let formatButton = document.querySelectorAll(".format");
+let scriptButton = document.querySelectorAll(".script");
+
+//List of fontList
+let fontList = ["Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Couries New",
+    "cursive",];
