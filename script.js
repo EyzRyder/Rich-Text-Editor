92ea15657dd56fe3b7c6c276c4aa4c68d7f8aee9
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2022-07-31 14:56:44 -0300

    Created function to highlight Clicked Buttons

--- script.js
+++ script.js
@@ -28,3 +28,31 @@ const initializer = () => {
     highlighter(formatButtons, false);
     highlighter(spacingButtons, true);
 }
+
+//Highlight clicked button
+const highlighter = (className, needsRemoval)=>{
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            //needsRemoval = true means only one button shoould be highlight and other would be normal
+            if (needsRemoval) {
+                let alreadyActive = false;
+
+                //If currently clicked buttons is already active
+                if (button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+
+                //remove highlight from other buttons
+                highlighterRemover(className);
+                if (!alreadyActive) {
+                    //highlight clicked button
+                    button.classList.add("active");
+                }
+            }
+            else { 
+                //if other buttons can be highlighted
+                button.classList.toggle("active");
+            }
+        });
+    })
+}
